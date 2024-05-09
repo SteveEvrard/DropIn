cb9c572e70bd9fd753cea590e8bf4f546a0b9cb8
Steve Evrard <stevelevrard@gmail.com>
2024-05-09 17:08:47 -0400

    Initial Commit

---
+++ DropIn/DropInApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropInApp.swift
+//  DropIn
+//
+//  Created by Stephen Evrard on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DropInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
